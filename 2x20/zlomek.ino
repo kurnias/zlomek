7071b6f6ee7f69a45013dd52e27f4401b59f4854
rysieklabus <rysieklabus@gmail.com>
2014-11-26 12:29:54 +0000

    v.1.1.5 working version

--- 2x20/zlomek.ino
+++ 2x20/zlomek.ino
@@ -6,6 +6,8 @@
 
 //************************************************************************//
 /* CHANGELOG (nowe na górze)
+ 2014.11.26 - v.1.1.5 Krok syntezy 50Hz/1kHz/10kHz wyczyszczony kod prawidłowe wyświetlanie na LCD
+ reklama na starcie syntezera
  2014.11.24 - v.1.1.4 Osobna gałąź przeznaczona dla klasycznego wyświetlacza 2x20
  2014.10.19 - v.1.0.4 wprowadzamy pośrednią kilka zmiennych do sekcji konfiguracji
  drobne czyszczenie kodu, poprawki w komentarzach
@@ -17,7 +19,7 @@
  2014.05.22 - pierwsza wersja kodu warsztaty arduino w komorowie.  
  */
 //************************************************************************//
-#define software_version "1.1.4"
+#define software_version "1.1.5"
 
 //podłączamy bibliotekę syntezera
 #include <AH_AD9850.h>
@@ -55,31 +57,48 @@ RotaryEncoder encoder(A0,A1,5,6,1000);
 
 //*****************************************************************************************************************************
 //zmienne do modyfikacji każdy ustawia to co potrzebuje
-const int pulses_for_groove = 2;             //ilość impulsów na ząbek enkodera zmienić w zależności od posiadanego egzemplarza
-const int step_input = A2;                   //wejście do podłączenia przełącznika zmiany kroku
-const long low_frequency_limit = 3500000;    //dolny limit częstotliwości
-const long high_frequency_limit = 3800000;   //górny limit częstotliwości
-const long start_frequency = 3715000;        //częstotliwość startowa
-const long posrednia = -8000000;             //częstotliwość pośredniej, każdy dobiera swoją w zależności od konstrukcji radia
+const int pulses_for_groove = 2;                         //ilość impulsów na ząbek enkodera zmienić w zależności od posiadanego egzemplarza
+const int step_input = A2;                               //wejście do podłączenia przełącznika zmiany kroku
+const long low_frequency_limit = 3500000;                //dolny limit częstotliwości
+const long high_frequency_limit = 3800000;               //górny limit częstotliwości
+const long start_frequency = 3715000;                    //częstotliwość startowa
+const long posrednia = -8000000;                         //częstotliwość pośredniej, każdy dobiera swoją w zależności od konstrukcji radia
 //*****************************************************************************************************************************
 
 //zmienne wewnętrzne, 
 //jeśli nie trzeba proszę nie modyfikować
-char buffor[] = "              ";            //zmienna pomocnicza do wyrzucania danych na lcd
-long frequency = start_frequency;            //zmienna dla częstotliwości, wstawiamy tam częstotliwość od której startujemy
-long step_value = 100;                       //domyślny krok syntezy
-int enc_sum = 0;                             //zmienna pomocnicza do liczenia impulsów z enkodera
+char buffor[] = "              ";                        //zmienna pomocnicza do wyrzucania danych na lcd
+long frequency = start_frequency;                        //zmienna dla częstotliwości, wstawiamy tam częstotliwość od której startujemy
+long step_value = 50;                                    //domyślny krok syntezy
+int enc_sum = 0;                                         //zmienna pomocnicza do liczenia impulsów z enkodera
 
 //funkcja do obsługi wyświetlania zmiany częstotliwości
 void show_frequency(){
-  sprintf(buffor,"%08lu",frequency);         //konwersja danych do wyświetlenia (ładujemy częstotliwość do stringa i na ekran)
-  lcd.print(buffor);                //wyświetlamy dane na lcd 
+  long mhz = frequency / 1000000;                        //wyciągam czyste MHz
+  long khz = frequency % 1000000;                        //wyciągam czyste kHz w dwóch krokach
+  khz = khz / 1000;                                      //kHz obcinam z Hz
+  long hz = frequency % 1000;                            //wyciągam Hz
+  hz = hz / 10;                                          //obcinam by wyświetlić tylko dziesiątki Hz
+  sprintf(buffor,"%1lu.%03lu.%02lu",mhz,khz,hz);         //konwersja danych do wyświetlenia (ładujemy częstotliwość, kropki itd do stringa i na ekran)
+  lcd.setCursor(6,1);                                    //ustawianie kursora kolumna,wiersz
+  lcd.print(buffor);                                     //wyświetlamy dane na lcd 
 }
 
 //funkcja do wyświetlania aktualnego kroku syntezera
 void show_step(){
-  sprintf(buffor,"%08lu",step_value);         //konwersja danych do wyświetlenia (ładujemy krok syntezy do stringa i na ekran)
-  lcd.print(buffor);                 //wyświetlamy dane na lcd (8 oznacza drugi rząd) 
+  switch(step_value){
+     case 50:
+       sprintf(buffor," 50Hz",step_value);         //bufor dla kroku 50Hz
+     break; 
+     case 1000:
+       sprintf(buffor," 1kHz",step_value);         //bufor dla kroku 1kHz
+     break;
+     case 10000:
+       sprintf(buffor,"10kHz",step_value);         //bufor dla kroku 10kHz
+     break;
+  }
+  lcd.setCursor(15,0);                             //ustawiam kursor w 15-tym rzędzie i 1-szym wierszu
+  lcd.print(buffor);                               //wyświetlam dane na lcd (8 oznacza drugi rząd) 
 }
 
 //funkcja zmieniajaca częstotliwość DDS-a
@@ -99,10 +118,17 @@ void set_frequency(int plus_or_minus){
 // setup funkcja odpalana przy starcie
 void setup(){  
   Serial.begin(9600);                         //uruchamiam port szeregowy w celach diagnostycznych       
-  lcd.begin(20, 2);
+  lcd.begin(20, 2);                           //inicjalizacja wyświetlacza 20 rzędów w 2 wierszach
   pinMode(step_input,INPUT_PULLUP);           //inicjalizujemy wejście zmiany kroku i podciągamy je do plusa
-  set_frequency(0);                           //odpalamy syntezer i ustawiamy częstotliwość startową 
-  delay(1000);                                //sekunda opóźnienia   
+  set_frequency(0);                           //odpalamy syntezer i ustawiamy częstotliwość startową
+  lcd.setCursor(0,0);                         //ustawiam kursor pierwszys rząd pierwszy wiersz
+  lcd.print("Zlomek DDS");                    //reklama produktu
+  lcd.setCursor(0,1);                         //ustawiam kursoe pierwszy rząd drugi wiersz
+  lcd.print("v.");                            //v jak wersja
+  lcd.print(software_version);                //i wyświetlam wersję
+  
+  delay(3000);                                //opóźnienie 
+  lcd.clear();                                //czyścimy wyświetlacz
   show_frequency();                           //pokazujemy częstotliwość na lcd
   show_step();                                //pokazujemy krok syntezy
 } 
@@ -119,17 +145,14 @@ void loop(){
    delay(100);                            //odczekajmy 100msec
     if(digitalRead(step_input) == LOW){   //jeśli klawisz nadal jest wcisnięty (czyli nie są to zakłócenia)
        switch(step_value){                //za pomocą instrukcji swich zmieniamy krok
-          case 100000:                    //jeśli krok jest 100kHz ustaw 100Hz
-            step_value = 100;
-          break;
           case 10000:                     //jeśli krok jest 10kHz ustaw 100kHz
-            step_value = 100000;
+            step_value = 1000;
           break;
           case 1000:                      //jeśli krok jest 1kHz ustaw 10kHz
-            step_value = 10000;
+            step_value = 50;
           break;
-          case 100:                       //jeśli krok jest 100Hz ustaw 1kHz
-            step_value = 1000;
+          case 50:                       //jeśli krok jest 100Hz ustaw 1kHz
+            step_value = 10000;
           break;
        }
     }
@@ -159,3 +182,4 @@ int freeRam () {
   return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
 }
 */
+
